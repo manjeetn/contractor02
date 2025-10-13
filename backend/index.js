e56fdea609276447c223cb0c35f5f13348bf0761
Manjeet Nishad <manjeet98100@gmail.com>
2025-10-13 12:39:30 +0530

    Fix CORS and update API routes for deployment

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,18 @@ import settingRoutes from "./routes/settingsRoute.js";
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+const allowedOrigins = [
+  "http://localhost:5173", 
+  "https://contractor-management-app.vercel.app",
+];
+
+app.use(
+   cors({
+    origin: allowedOrigins,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 
--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -122,7 +122,7 @@ router.post('/login', async (req, res) => {
     }
 
     const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) return res.status(400).json({ message: 'Password Incorrect. Please Enter Correct PAssword' });
+    if (!isMatch) return res.status(400).json({ message: 'Password Incorrect. Please Enter Correct Password' });
 
     const payload = {
       userId: user._id,
