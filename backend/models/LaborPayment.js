11126f83670abaf6190ba76edcab387fa0e562ea
Manjeet Nishad <manjeet98100@gmail.com>
2026-02-11 15:26:39 +0530

    LaborPaymnt updated

--- backend/models/LaborPayment.js
+++ backend/models/LaborPayment.js
@@ -1,11 +1,19 @@
 import mongoose from "mongoose";
 
 const LaborPaymentSchema = new mongoose.Schema({
-  laborId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  amount: { type: Number, required: true },
-  note: { type: String, default: "" },
-  date: { type: Date, default: Date.now },
-  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }, // admin who created
+  laborId: { type: mongoose.Schema.Types.ObjectId, 
+    ref: "User",
+   required: true },
+  amount: { type: Number,
+     required: true },
+  note: { type: String,
+   default: "" },
+  date: { type: Date,
+     default: Date.now
+     },
+  createdBy: { type: mongoose.Schema.Types.ObjectId,
+     ref: "User",
+      required: true }, 
 }, { timestamps: true });
 
 const LaborPayment = mongoose.model("LaborPayment", LaborPaymentSchema);
